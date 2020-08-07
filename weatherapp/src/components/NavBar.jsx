c20b76cb5ee1d72d8be22cb23c6318a525196341
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-08-07 17:22:16 +0300

    Create NavBar.jsx

---
+++ weatherapp/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import {Navbar, Container} from 'react-bootstrap'
+
+const NavBar =()=>{
+  return(
+    
+ 
+      <Navbar expand="lg" variant="dark" bg="dark">
+      <Container>
+        <Navbar.Brand href="#">World Weather</Navbar.Brand>
+      </Container>
+  </Navbar>
+ )
+}
+
+export default NavBar 
