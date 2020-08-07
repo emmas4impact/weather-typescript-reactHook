5bfbc0687bc2fb099f5f6e78c650b09be51112c1
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-08-07 17:22:21 +0300

    Update App.tsx

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import Weather from './components/Weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import NavBar from './components/NavBar'
 function App() {
   return (
     <div className="App">
-     
+        <NavBar/>
         <Weather title="Weather Report"/>
     </div>
   );
